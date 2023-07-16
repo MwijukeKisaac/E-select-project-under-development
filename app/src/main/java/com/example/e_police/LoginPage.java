604dd7a05b669e539efa90b0f4399903a8c0c3d0
MwijukyeKisaac <mwijukyekisaac@gmail.com>
2023-07-16 13:26:49 +0300

    more pages linked

--- app/src/main/java/com/example/e_police/LoginPage.java
+++ app/src/main/java/com/example/e_police/LoginPage.java
@@ -2,7 +2,9 @@ package com.example.e_police;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+    }
+
+    public void GoToCreateAccount(View view) {
+        Intent intent = new Intent(LoginPage.this, CreateAccount.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void GoToForgotAccount(View view) {
+        Intent intent = new Intent(LoginPage.this, ForgotPassword.class);
+        startActivity(intent);
+    }
+
+    public void GoT0CA(View view) {
+        Intent intent = new Intent(LoginPage.this, CreateAdvanceUserAccount.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/e_police/ReportingPage.java
@@ -0,0 +1,14 @@
+package com.example.e_police;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reporting_page);
+    }
+}
